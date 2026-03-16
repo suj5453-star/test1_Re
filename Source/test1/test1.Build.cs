951ed474401f329cd8a8939623aad9278267649f
SEOWOOJEONG <unha1@naver.com>
2026-03-16 17:10:26 +0900

    add : TChatWidget(UUserWidget) 생성

--- Source/test1/test1.Build.cs
+++ Source/test1/test1.Build.cs
@@ -8,9 +8,9 @@ public class test1 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "SlateCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "test1" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
